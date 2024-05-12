dcff954669a8b109f56ac69cffd27b0d377b8eba
jdevc920 <jdcamarin20@gmail.com>
2024-05-12 13:15:39 +0800

    [*] Home content addition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import mapimg from '../assets/hiligaynon_map.png';
+
 function Home() {
     return (
         <div className="container mt-4">
@@ -10,6 +12,27 @@ function Home() {
                     a student, or simply curious, let us guide you through the nuances and beauty of Hiligaynon/Ilonggo."
                 </p>
             </div>
+            <div className="container home-landing-message mt-2">
+                <div className="row mt-2 distribution">
+                    <div className="col-md-3 ">
+                        <img src={mapimg} alt="Map" className="map-image" />
+                    </div>
+                    <div className="col-md-9">
+                        <div className="container">
+                            <h3 className="mt-2">Geographic Distribution</h3>
+                            <p className="dis-par" >
+                                "Hiligaynon is primarily spoken in the Western Visayas region, including Iloilo, Capiz, Guimaras, and Negros Occidental.
+                                It is also found in South Cotabato (including General Santos), Sultan Kudarat, and North Cotabato in Soccsksargen. Additionally,
+                                it is spoken in neighboring provinces such as Antique and Aklan in Western Visayas, Negros Oriental in Central Visayas, and parts
+                                of Mindoro, Romblon, and Palawan in Mimaropa."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div >
+
+
+
             
         </div>
   );
